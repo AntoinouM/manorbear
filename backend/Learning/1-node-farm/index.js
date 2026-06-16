d1f3571f735e2dca49febf9cc095f9b3dc32117a
Antoine Muneret <antoine.muneret2@gmail.com>
2026-06-16 13:04:40 +0200

    Section 2 - 10: Add asynchronous file reading and writing in Node.js using callbacks

--- backend/Learning/1-node-farm/index.js
+++ backend/Learning/1-node-farm/index.js
@@ -1,2 +1,29 @@
-const hello = 'Hello world';
-console.log(hello);
+const fs = require('fs');
+
+// Blocking, synchronous way
+// const textInput = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
+// console.log(textInput);
+
+// const textOutput = `This is what we know about the avocado: ${textInput}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./starter/txt/output.txt', textOutput);
+// console.log('File written.');
+
+// Non-blocking, asynchronous way
+fs.readFile('./starter/txt/start.txt', 'utf-8', (error, data1) => {
+  fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (error, data2) => {
+    console.log(data2);
+    fs.readFile('./starter/txt/append.txt', 'utf-8', (error, data3) => {
+      console.log(data3);
+
+      fs.writeFile(
+        './starter/txt/final.txt',
+        `${data2}\n${data3}`,
+        'utf-8',
+        (err) => {
+          console.log('Your file has been written :)');
+        },
+      );
+    });
+  });
+});
+console.log('Will read file!');
