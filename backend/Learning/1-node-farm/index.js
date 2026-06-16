758ff0665ee4f73f77a4f1b9de66a0aeb0a6a2f3
Antoine Muneret <antoine.muneret2@gmail.com>
2026-06-16 13:27:44 +0200

    Section 2  - part 11: Add basic HTTP server to respond with a message

--- backend/Learning/1-node-farm/index.js
+++ backend/Learning/1-node-farm/index.js
@@ -1,4 +1,11 @@
 const fs = require('fs');
+const http = require('http');
+
+/****************************
+ ******** SECTION 2 *********
+ ****************************/
+
+/* --------- FILES (part 10) ---------  */
 
 // Blocking, synchronous way
 // const textInput = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
@@ -9,22 +16,34 @@ const fs = require('fs');
 // console.log('File written.');
 
 // Non-blocking, asynchronous way
-fs.readFile('./starter/txt/start.txt', 'utf-8', (error, data1) => {
-  if (error) return console.log(error.message);
-  fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (error, data2) => {
-    console.log(data2);
-    fs.readFile('./starter/txt/append.txt', 'utf-8', (error, data3) => {
-      console.log(data3);
+// fs.readFile('./starter/txt/start.txt', 'utf-8', (error, data1) => {
+//   if (error) return console.log(error.message);
+//   fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (error, data2) => {
+//     console.log(data2);
+//     fs.readFile('./starter/txt/append.txt', 'utf-8', (error, data3) => {
+//       console.log(data3);
 
-      fs.writeFile(
-        './starter/txt/final.txt',
-        `${data2}\n${data3}`,
-        'utf-8',
-        (err) => {
-          console.log('Your file has been written :)');
-        },
-      );
-    });
-  });
+//       fs.writeFile(
+//         './starter/txt/final.txt',
+//         `${data2}\n${data3}`,
+//         'utf-8',
+//         (err) => {
+//           console.log('Your file has been written :)');
+//         },
+//       );
+//     });
+//   });
+// });
+// console.log('Will read file!');
+
+/* --------- SERVER (part 11) ---------  */
+
+const server = http.createServer((req, res) => {
+  //console.log(req);
+  res.end('Hello from the server!');
 });
-console.log('Will read file!');
+
+// here, the second arg is optional and would default to localhost with usually IP address: '127.0.0.1'
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+});
